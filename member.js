c1f3428c769bf08888ace94ae15df24798d7cf28
gnagasri <gnagasrivarma2809@gmail.com>
2025-01-06 11:15:49 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,19 @@
+function skillsMember()
+{
+    var member = new Member();
+    member.skills = ["JavaScript", "Java", "C++"];
+    console.log(member.skills);
+}
+
+function Member()   
+{
+    this.skills = [];
+}   
+skillsMember();
+
+fuctiion addioData
+{
+    console.log("Addio data");
+}
+
+addioData();
